b873af564251c8532db09f859b39d813427ac685
Pagrune <pagrune@orange.fr>
2023-03-22 22:14:29 +0100

    style vue connexion

--- vue/vue_connexion.php
+++ vue/vue_connexion.php
@@ -12,7 +12,7 @@
             </label>
             <input id="me_connecter" name="me_connecter" type="submit" value="Me connecter">
         </form>
-        <div>
+        <div class="no_account">
             <p>
                 Je n'ai pas de compte ?
             </p>
