f56e84eab03f89f3713094e3f40ccd9baa363a54
Pagrune <pagrune@orange.fr>
2023-03-22 21:12:31 +0100

    Vue connexion contenu

---
+++ vue/vue_connexion.php
@@ -0,0 +1,27 @@
+<div id="connexion">
+    <div >
+        <h1>
+            Connexion
+        </h1>
+        <form action="">
+            <label for="identifiant">
+                <input type="text" id="identifiant" name="identifiant" placeholder="Votre email d'identification" >
+            </label>
+            <label for="identifiant">
+                <input type="email" id="identifiant" name="identifiant" placeholder="Votre email d'identification" >
+            </label>
+            <label for="identifiant">
+                <input type="password" id="mdp_connex" name="mdp_connex" placeholder="Votre mot de passe" >
+            </label>
+            <input id="me_connecter" name="me_connecter" type="submit" value="Me connecter">
+        </form>
+        <div>
+            <p>
+                Je n'ai pas de compte ?
+            </p>
+            <a href="">
+                Créer votre compte
+            </a>
+        </div>
+    </div>
+</div>
