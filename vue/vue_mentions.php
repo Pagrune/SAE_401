b1602f4479fbae738ea1ebd08ac508286ee8cc8a
Pagrune <pagrune@orange.fr>
2023-03-22 23:56:47 +0100

    début contenu vue mention

---
+++ vue/vue_mentions.php
@@ -0,0 +1,8 @@
+<div id="mention">
+    <h1>
+        Mentions légales
+    </h1>
+    <p>
+    Le présent site web est édité par We-Escape GmbH, société par actions simplifiée, immatriculée au registre du commerce et des sociétés de Freiburg im Breisgau sous le numéro HRB 725194.
+    </p>
+</div>
