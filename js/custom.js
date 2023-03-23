d70141393ece86c457f3ff8356b967cb52168f88
Pagrune <pagrune@orange.fr>
2023-03-23 16:12:39 +0100

    contenu + style mobile vue aventure part 1

--- js/custom.js
+++ js/custom.js
@@ -13,3 +13,46 @@ function toggle_menu(){
     }
 }
 
+var currentDate = new Date();
+        let jours = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"]
+        currentDate.setDate(1);
+
+        function AfficherMois() {
+            var temp = new Date(currentDate);
+            var output = "";
+            for (var i = 0, fin = FinDuMois(); i < fin; i++) {
+                output += `
+                    <div class="${jours[temp.getDay()]}" data-date="${temp.toLocaleDateString()}">
+                        <form action='' method='post'>
+                        <input type="checkbox" id="date_jour" name="date_jour" value="true">
+                        <div>${temp.toLocaleDateString()}</div>
+                    
+                    </div>`
+                temp.setDate(temp.getDate() + 1);
+            }
+            document.querySelector(".cal").innerHTML = output;
+            // document.querySelectorAll("textarea").forEach(function (e) {
+            //     e.addEventListener("change", saveData)
+            // })
+        }
+
+
+        function FinDuMois() {
+            var temp = new Date(currentDate.getYear(), currentDate.getMonth() + 1, 0);
+            return temp.getDate();
+        }
+
+
+        document.querySelector(".btn_plus").addEventListener("click", function () {
+            currentDate.setMonth(currentDate.getMonth() + 1);
+            AfficherMois();
+        })
+        document.querySelector(".btn_moins").addEventListener("click", function () {
+            currentDate.setMonth(currentDate.getMonth() - 1);
+            AfficherMois();
+        })
+
+        
+        // document.querySelector(".month").innerHTML = currentDate.getMonth();
+
+        AfficherMois();
---
+++ vue/vue_aventure.php
@@ -0,0 +1,277 @@
+<div id="aventure_solo">
+    <div class="block-1">
+        <img src="img/kredo/kredo.png" alt="escape game Kredo castello">
+        <div class="block_carac">
+            <h1>
+                Kredo Castello
+            </h1>
+            <h2>
+                Caractéristiques
+            </h2>
+            <div>
+                <div class="genre">
+                    <p>
+                        Genre :
+                    </p>
+                    <strong>Fantastique</strong>
+                </div>
+                <div class="genre">
+                   <strong>
+                    Outdoor
+                   </strong>
+                   <p>
+                    Escape
+                   </p>
+                </div>
+            </div>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#3E3535" class="bi bi-clock" viewBox="0 0 16 16">
+                    <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
+                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+                </svg>
+                <p>
+                    Durée : 3 heures | environ 4 km</p>
+            </div>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#3E3535" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                </svg>
+                <p>
+                    à 
+                    Ihringen
+                </p>
+            </div>
+            <p class="dif_aventure">
+                <span>
+                    Convient aux joueurs débutants
+                </span>
+            </p>
+        </div>
+    </div>
+    <div class="block-2">
+        <div class="descr">
+            <div class="left-column">
+                <p>
+                    "Der Codex" se compose de 3 aventures qui se déroulent dans différents endroits de Kaiserstuhl à partir de mai 2023. Commandez maintenant ce coupon et sécurisez-vous pour 110 € par personne les 3 aventures en une seule fois.
+                </p>
+                <p>
+                    Voyagez dans des époques différentes et plongez-vous dans les histoires de "Kredo Castello", "Spiritus Sancti" et "Vinea Flamara". Découvrez où se cache le secret - Qu'est-ce que le Codex ? Vous serez époustouflé - promis !
+                </p>
+            </div>
+            <div class="rigt-column">
+                <p>
+                    Une aventure sauvage vous attend et nous vous promettons que des choses se produiront auxquelles vous ne vous attendiez pas !
+                </p>
+                <p>
+                    Nous ne vous en dirons pas trop ! Réservez ce coupon et vivez l'aventure Escape la plus extraordinaire et la plus grande du monde !
+                </p>
+            </div>
+        </div>
+        <img src="img/page_aventure_solo/coridor.jpg" alt="">
+    </div>
+    <div class="block-3">
+        <div class="categorie_prix">
+            <h2>
+                Catégories de prix
+            </h2>
+            <table>
+               <tr>
+                <td>Prix de groupe avec 2 - 3 personnes</td>
+                <td>69,00€</td>
+               </tr>
+               <tr>
+                <td>Prix de groupe avec 4 personnes</td>
+                <td>91,00€</td>
+               </tr>
+               <tr>
+                <td>Prix de groupe avec 5 personnes</td>
+                <td>114,00€</td>
+               </tr>
+               <tr>
+                <td>Prix de groupe avec 6 personnes</td>
+                <td>135,00€</td>
+               </tr>
+               <tr>
+                <td>Prix de groupe avec 7 personnes</td>
+                <td>158,00€</td>
+               </tr>
+               <tr>
+                <td>Prix de groupe avec 8 personnes</td>
+                <td>178,00€</td>
+               </tr>
+               <tr>
+                <td>Prix de groupe avec 9 personnes</td>
+                <td>198,00€</td>
+               </tr>
+               <tr>
+                <td>Prix de groupe avec 10 personnes</td>
+                <td>219,00€</td>
+               </tr>
+               <tr>
+                <td>Prix de groupe avec 11 personnes</td>
+                <td>240,00€</td>
+               </tr>
+               <tr>
+                <td>Prix de groupe avec 12 personnes</td>
+                <td>261,00€</td>
+               </tr>
+               <tr>
+                <td>Prix de groupe avec plus de 12 personnes</td>
+                <td>299,00€</td>
+               </tr>
+            </table>
+        </div>
+        <div class="creneaux">
+            <h2>
+                Créneaux disponibles
+            </h2>
+            <div>
+                <div class="creneau_offre">
+                    <button class="offrir">
+                        <a href="index.php?action=cadeaux">Offrez l'aventure à quelqu'un</a>
+                    </button>
+                    <p>
+                        ou
+                    </p>
+                </div>
+                <div class="resa_aventure">
+                    <h3>Réserver votre aventure</h3>
+                    <div>
+                        <div class="choix jour">
+                            <img src="img/icons/time-and-calendar.png" alt="icone calendrier">
+                            <p>
+                                Choisir votre jour
+                            </p>
+                        </div>
+                        <div class="toggle toggle-calendar">
+                            <button class=btn_moins>-</button>
+                            <!-- <p class="month"></p> -->
+                            <button class=btn_plus>+</button>
+                            <div class="cal"></div>
+                        </div>
+                    </div>
+                    <div class="choix creneau">
+                        <img src="img/icons/horloge.png" alt="icone horraire">
+                        <p>
+                            Choisir votre créneau
+                        </p>
+                    </div>
+                    <div class="toggle choix_horraire">
+                        <form action="" method="post">
+                            <div>
+                                <input type="checkbox" id="heure_jour" name="heure_jour">
+                                <label for="">9h</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="heure_jour" name="heure_jour">
+                                <label for="">10h</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="heure_jour" name="heure_jour">
+                                <label for="">11h</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="heure_jour" name="heure_jour">
+                                <label for="">12h</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="heure_jour" name="heure_jour">
+                                <label for="">13h</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="heure_jour" name="heure_jour">
+                                <label for="">14h</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="heure_jour" name="heure_jour">
+                                <label for="">15h</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="heure_jour" name="heure_jour">
+                                <label for="">16h</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="heure_jour" name="heure_jour">
+                                <label for="">17h</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="heure_jour" name="heure_jour">
+                                <label for="">18h</label>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="choix groupe">
+                        <img src="img/icons/contact.png" alt="icone groupe">
+                        <p>
+                            Choisir le nombre de personnes
+                        </p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+    </div>
+    <div class="block-avantage">
+        <h2>
+            Les avantages de la réservation
+        </h2>
+        <div class="grid_avantage">
+            <div class="icon_avant">
+                <img src="img/icons/thunder.png" alt="icone éclair">
+                <div>
+                    <span>Réservation rapide et simple</span>
+                    <p>
+                        Réservez votre activité en quelques minutes seulement
+                    </p>
+                </div>
+            </div>
+            <div class="icon_avant">
+                <img src="img/icons/lhorloge.png" alt="icone horloge">
+                <div>
+                    <span>Accessible 24h/24, 7j/7</span>
+                    <p>
+                        Notre site Web est ouvert jour et nuit
+                    </p>
+                </div>
+            </div>
+            <div class="icon_avant">
+                <img src="img/icons/pile-de-pieces.png" alt="icone piece de monnaie">
+                <div>
+                    <span>Prix garantis</span>
+                    <p>
+                        Pas de frais supplémentaire
+                    </p>
+                </div>
+            </div>
+            <div class="icon_avant">
+                <img src="img/icons/email.png" alt="icone enveloppe">
+                <div>
+                    <span>Confirmation immédiate</span>
+                    <p>
+                        Les billets sont envoyés sur votre adresse e-mail
+                    </p>
+                </div>
+            </div>
+            <div class="icon_avant">
+                <img src="img/icons/cadenas-verrouille.png" alt="icone cadenas">
+                <div>
+                    <span>Paiement en ligne</span>
+                    <p>
+                        100 % sécurisé avec protocole de sécurisation SSL
+                    </p>
+                </div>
+            </div>
+            <div class="icon_avant">
+                <img src="img/icons/smiley.png" alt="icone smiley">
+                <div>
+                    <span>Pas de stress</span>
+                    <p>
+                        Votre place est réservée
+                    </p>
+                </div>
+            </div>         
+        </div>
+    </div>
+</div>
