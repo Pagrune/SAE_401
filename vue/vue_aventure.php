cb5a8d2c655f50dfa72a9fc8c3a53aa768b6578a
Pagrune <pagrune@orange.fr>
2023-03-23 17:32:42 +0100

    vue aventure style

--- vue/vue_aventure.php
+++ vue/vue_aventure.php
@@ -8,7 +8,7 @@
             <h2>
                 Caractéristiques
             </h2>
-            <div>
+            <div class="grid_genre">
                 <div class="genre">
                     <p>
                         Genre :
@@ -24,7 +24,7 @@
                    </p>
                 </div>
             </div>
-            <div>
+            <div class="flexitude">
                 <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#3E3535" class="bi bi-clock" viewBox="0 0 16 16">
                     <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
                     <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
@@ -32,7 +32,7 @@
                 <p>
                     Durée : 3 heures | environ 4 km</p>
             </div>
-            <div>
+            <div class="flexitude">
                 <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#3E3535" class="bi bi-geo-alt" viewBox="0 0 16 16">
                     <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
                     <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
@@ -206,6 +206,19 @@
                         <p>
                             Choisir le nombre de personnes
                         </p>
+                    </div>
+                    <div>
+                        <div class="groupe_choix">
+                            <input type="checkbox" id="taille_groupe" name="taille_groupe">
+                            <label for="">
+                                <p>
+                                    Prix de groupe avec 2 - 3 personnes
+                                </p>
+                                <p>
+                                    69,00€
+                                </p>
+                            </label>
+                        </div>
                     </div>
                 </div>
                 
