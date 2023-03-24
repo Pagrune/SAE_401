1ce79091d472724b875c90f868698f4abbc46373
Pagrune <pagrune@orange.fr>
2023-03-24 14:19:45 +0100

    début contenu vue panier

--- vue/vue_panier.php
+++ vue/vue_panier.php
@@ -1,3 +1,43 @@
-<div>
+<div id="panier">
+    <h1>Mon panier</h1>
+    <div>
+
+    </div>
+    <div class="paiement">
+        <div>
+            <h2>Paiement</h2>
+            <div>
+                <img src="" alt="">
+                <img src="" alt="">
+            </div>
+        </div>
+        <div>
+            <div>
+                <form action="">
+                    <div>
+                        <input type="text" class="numero" placeholder="0000 0000 0000 0000">
+                        <div>
+                            <p>
+                                Expire / fin
+                            </p>
+                            <input type="text" class="date" placeholder="MM/AA">
+                        </div>
+				        <input type="text" class="nom" placeholder="M(e) PRENOM NOM">
+                        <div>
+                            <p>Numéro secret</p>
+                            <input type="number" placeholder="000">
+                        </div>
+                    </div>
+                    <input type="submit" value="Confirmer mon paiement">
+                </form>
+                <h3>
+                    Récapitulatif de la commande
+                </h3>
+                <p>
+                    69€
+                </p>
+            </div>
+        </div>
+    </div>
 
 </div>
