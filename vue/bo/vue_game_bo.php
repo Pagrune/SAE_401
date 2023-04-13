cfc5a05ba40b0fe93628e5cb5f30117db0635080
Pagrune <pagrune@orange.fr>
2023-04-13 16:15:44 +0200

    vue jeux

--- vue/bo/vue_game_bo.php
+++ vue/bo/vue_game_bo.php
@@ -1,5 +1,7 @@
-<a href="bo.php">home</a>
-<a href='bo.php?action=logout'>déconnexion</a>
+<h1>
+    Game management
+</h1>
+<div id="game_management">
 
 <?php
 foreach($games as $game){
@@ -13,17 +15,18 @@ foreach($games as $game){
         <option value="suspens">suspense</option>
         <option value="drame">drame</option>
     </select>
+    </label>
 
-<br>
+ 
     
     <label for="">game duration
         <input type="text" value='<?=$game['game_duree']?>' required name='duree'>
     </label>
-    <br>
+     
     <label for="">game place
         <input type="text" value='<?=$game['game_lieu']?>' name='lieu' required>
     </label>
-    <br>
+     
     <label for="">game category
         <select name="categorie" id="" required>
     <option value="<?=$game['game_categorie']?>"><?=$game['game_categorie']?></option>
@@ -31,118 +34,124 @@ foreach($games as $game){
     <option value="novice">novice</option>
     <option value="amateur">amateur</option>
     <option value="expert">expert</option>
-</select>
+    </select>
     </label>
-    <br>
+     
     <label for="">game nbjoueur
         <input type="text" value='<?=$game['game_nbjoueur']?>' name='nbjoueur' required>
     </label>
-    <br>
+     
     <label for="">game environnement fr
         <input type="text" value='<?=$game['game_environnement']?>' name='environnement' required>
     </label>
-    <br>
+     
     <label for="">game environnement eng
         <input type="text" value='<?=$game['game_environnementeng']?>' name='environnementeng' required>
     </label>
-    <br>
+     
     <label for="">game nom
         <input type="text" value='<?=$game['game_nom']?>' name='nom' required>
     </label>
-    <br>
+     
     <label for="">game nom eng
         <input type="text" value='<?=$game['game_nomeng']?>' name='nomeng' required>
     </label>
-    <br>
-    <label for="">game description
-        <input type="text" value='<?=$game['game_description']?>' name='description' required>
+     
+    <label class="desc" for="">game description
+        <textarea name="description" cols="30" rows="10"><?=$game['game_description']?></textarea>
     </label>
-    <br>
-    <label for="">game description eng
-        <input type="text" value='<?=$game['game_decriptioneng']?>' name='decriptioneng' required>
+     
+    <label class="desc" for="">game description eng
+        <textarea name="decriptioneng" cols="30" rows="10"><?=$game['game_decriptioneng']?></textarea>
     </label>
-    <br>
+     
     <label for="">game prix
         <input type="text" value='<?=$game['game_prix']?>' name='prix' required>
     </label>
-    <br>
+     
     <label for="">game parcours
         <input type="text" value='<?=$game['game_parcours']?>' name='parcours' required>
     </label>
-    <br>
+     
     <label for="">game nbenigme
         <input type="text" value='<?=$game['game_nbenigme']?>' name='nbenigme' required>
     </label>
-    <br>
-    <label for="">game latitude
-        <input type="text" value='<?=$game['game_latitude']?>' name='latitude' required>
-    </label>
-    <br>
-    <label for="">game longitude
-        <input type="text" value='<?=$game['game_longitude']?>' name='longitude' required>
-    </label>
-    <br>
+     
+    <div class="coordinates">
+        <label for="">game latitude
+            <input type="text" value='<?=$game['game_latitude']?>' name='latitude' required>
+        </label>
+        
+        <label for="">game longitude
+            <input type="text" value='<?=$game['game_longitude']?>' name='longitude' required>
+        </label>
+    </div>
+    
+     
     <label for="">game prix 3
         <input type="text" value='<?=$game['game_prix_3']?>' name='prix_3' required>
     </label>
-    <br>
+     
     <label for="">game prix 4
         <input type="text" value='<?=$game['game_prix_4']?>' name='prix_4' required>
     </label>
-    <br>
+     
     <label for="">game prix 5
         <input type="text" value='<?=$game['game_prix_5']?>' name='prix_5' required>
     </label>
-    <br>
+     
     <label for="">game prix 6
         <input type="text" value='<?=$game['game_prix_6']?>' name='prix_6' required>
     </label>
-    <br>
+     
     <label for="">game prix 7
         <input type="text" value='<?=$game['game_prix_7']?>' name='prix_7' required>
     </label>
-    <br>
+     
     <label for="">game prix 8
         <input type="text" value='<?=$game['game_prix_8']?>' name='prix_8' required>
     </label>
-    <br>
+     
     <label for="">game prix 9
         <input type="text" value='<?=$game['game_prix_9']?>' name='prix_9' required>
     </label>
-    <br>
+     
     <label for="">game prix 10
         <input type="text" value='<?=$game['game_prix_10']?>' name='prix_10' required>
     </label>
-    <br>
+     
     <label for="">game prix 11
         <input type="text" value='<?=$game['game_prix_11']?>' name='prix_11' required>
     </label>
-    <br>
+     
     <label for="">game prix 12
         <input type="text" value='<?=$game['game_prix_12']?>' name='prix_12' required>
     </label>
-    <br>
+
     <label for="">game prix groupe
         <input type="text" value='<?=$game['game_prix_groupe']?>' name='prix_groupe' required>
     </label>
-    <br></br>
+
     <label>changer la miniature de l'escape game</label>
         <input type="hidden" name="MAX_FILE_SIZE" value="100000000">
         <input type="file" name="image_escape" class="texte" accept="image/png">
         
-</br></br>
     <label>changer l'image secondaire du jeu</label>
     <input type="hidden" name="MAX_FILE_SIZE" value="100000000">
     <input type="file" name="image_contexte" class="texte" accept="image/png">
 
+    <div class="confirm_change">
+        <input type="submit" value='Confirm' required>
+        
+        <a href="bo.php?action=delete_game&game=<?=$game['game_id']?>">Delete game</a>
+    </div>
+   
     
-</br></br>
-    <input type="submit" value='valider' required>
-    </br></br></br>
-    <a href="bo.php?action=delete_game&game=<?=$game['game_id']?>">delete game</a>
-    </form>
+</form>
 
 
 
 <?php
 }?>
+
+</div>
--- vue/bo/vue_new_game.php
+++ vue/bo/vue_new_game.php
@@ -1,7 +1,9 @@
-<a href="bo.php">home</a>
-<a href='bo.php?action=logout'>déconnexion</a>
+<h1>
+    New Game
+</h1>
 
-
+<div id="game_new">
+    
 <form action="bo.php?action=new_game" enctype="multipart/form-data" method='post'>
     <label for="">game_genre <select name="game_genre" id="">
             <option value="fantastique">fantastique</option>
@@ -9,16 +11,16 @@
             <option value="drame">drame</option>
         </select>
 
-        <br>
+         
 
         <label for="">game duration
-            <input type="text" value='' required name='duree'>
+            <input type="text" value='' placeholder="hours" required name='duree'>
         </label>
-        <br>
+         
         <label for="">game place
             <input type="text" value='' name='lieu' required>
         </label>
-        <br>
+         
         <label for="">game category
             <select name="categorie" id="" required>
                 <option value="familial">familial</option>
@@ -27,111 +29,112 @@
                 <option value="expert">expert</option>
             </select>
         </label>
-        <br>
+         
         <label for="">game nbjoueur
             <input type="text" value='' name='nbjoueur' required>
         </label>
-        <br>
+         
         <label for="">game environnement fr
             <input type="text" value='' name='environnement' required>
         </label>
-        <br>
+         
         <label for="">game environnement eng
             <input type="text" value='' name='environnementeng' required>
         </label>
-        <br>
+         
         <label for="">game nom
             <input type="text" value='' name='nom' required>
         </label>
-        <br>
+         
         <label for="">game nom eng
             <input type="text" value='' name='nomeng' required>
         </label>
-        <br>
+         
         <label for="">game description
-            <input type="text" value='' name='description' required>
+            <textarea name="description" cols="30" rows="10"></textarea>
         </label>
-        <br>
+         
         <label for="">game description eng
-            <input type="text" value='' name='decriptioneng' required>
+            <textarea name="decriptioneng" cols="30" rows="10"></textarea>
         </label>
-        <br>
+         
         <label for="">game prix
             <input type="text" value='' name='prix' required>
         </label>
-        <br>
+         
         <label for="">game parcours
             <input type="text" value='' name='parcours' required>
         </label>
-        <br>
+         
         <label for="">game nbenigme
             <input type="text" value='' name='nbenigme' required>
         </label>
-        <br>
+         
         <label for="">game latitude
             <input type="text" value='' name='latitude' required>
         </label>
-        <br>
+         
         <label for="">game longitude
             <input type="text" value='' name='longitude' required>
         </label>
-        <br>
+         
         <label for="">game prix 3
             <input type="text" value='' name='prix_3' required>
         </label>
-        <br>
+         
         <label for="">game prix 4
             <input type="text" value='' name='prix_4' required>
         </label>
-        <br>
+         
         <label for="">game prix 5
             <input type="text" value='' name='prix_5' required>
         </label>
-        <br>
+         
         <label for="">game prix 6
             <input type="text" value='' name='prix_6' required>
         </label>
-        <br>
+         
         <label for="">game prix 7
             <input type="text" value='' name='prix_7' required>
         </label>
-        <br>
+         
         <label for="">game prix 8
             <input type="text" value='' name='prix_8' required>
         </label>
-        <br>
+         
         <label for="">game prix 9
             <input type="text" value='' name='prix_9' required>
         </label>
-        <br>
+         
         <label for="">game prix 10
             <input type="text" value='' name='prix_10' required>
         </label>
-        <br>
+         
         <label for="">game prix 11
             <input type="text" value='' name='prix_11' required>
         </label>
-        <br>
+         
         <label for="">game prix 12
             <input type="text" value='' name='prix_12' required>
         </label>
-        <br>
+         
         <label for="">game prix groupe
             <input type="text" value='' name='prix_groupe' required>
         </label>
-        <br></br>
-        <label>changer la miniature de l'escape game</label>
+
+        <label>changer la miniature de l'escape game
         <input type="hidden" name="MAX_FILE_SIZE" value="100000000">
         <input type="file" name="image_escape" class="texte" accept="image/png" required>
+        </label>
 
-        </br></br>
-        <label>changer l'image secondaire du jeu</label>
+        <label>changer l'image secondaire du jeu
         <input type="hidden" name="MAX_FILE_SIZE" value="100000000">
         <input type="file" name="image_contexte" class="texte" accept="image/png" required>
+        </label>
 
 
         </br></br>
-        <input type="submit" value='valider' name='valider' required>
-        </br></br></br>
+        <input type="submit" value='Confirm' name='valider' required>
 
-</form>
+    </form>
+</div>
